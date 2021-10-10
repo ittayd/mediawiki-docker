b7241bba0b64101b8dc96fc68ab54a82757dd47d
Ittay Dror <idror@akamai.com>
2021-10-10 20:27:03 +0300

    added onedrive sync. not working - cannot specify a destination path in onedrive remote

--- docker-entrypoint-debian.sh
+++ docker-entrypoint-debian.sh
@@ -14,4 +14,43 @@ FILE=/var/www/html/LocalSettings.php
     fi
 }) &
 
+# check a refresh token exists
+if [ -f /config/refresh_token ]; then
+  echo "Found onedrive refresh token..."
+else
+  echo
+  echo "-------------------------------------"
+  echo "ONEDRIVE LOGIN REQUIRED"
+  echo "-------------------------------------"
+  echo "To use this container you must authorize the OneDrive Client."
+
+  if [ -t 0 ] ; then
+    echo "-------------------------------------"
+    echo
+  else
+    echo
+    echo "Please re-start start the container in interactive mode using the -it flag:"
+    echo
+    echo "docker run -it -v /local/config/path:/config -v /local/documents/path:/documents oznu/onedrive"
+    echo
+    echo "Once authorized you can re-create container with interactive mode disabled."
+    echo "-------------------------------------"
+    echo
+    exit 1
+  fi
+
+fi
+
+# turn on or off verbose logging
+if [ "$DEBUG" = "1" ]; then
+  VERBOSE=true
+else
+  VERBOSE=false
+fi
+
+echo "Starting onedrive client..."
+
+onedrive --single-directory Mediawiki --syncdir=/var/www/data --verbose=${VERBOSE} --monitor
+
+echo "Starting Apache"
 apache2-foreground
