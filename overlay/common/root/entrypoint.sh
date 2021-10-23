678222459988ef99373e4d3bd9c6774807de64fa
Ittay Dror <idror@akamai.com>
2021-10-23 18:30:35 +0300

    https with self signed cert

--- overlay/common/root/entrypoint.sh
+++ overlay/common/root/entrypoint.sh
@@ -3,7 +3,7 @@
 /prepare.sh 
 
 # need to run with /bin/bash since the s6 wrapper doesn't work yet. 
-/bin/bash /etc/services.d/onedrive/run check || exit 1
+/bin/bash /etc/services.d/onedrive/run check || exit 1 
 
 /backup.sh restore
 
--- overlay/common/root/prepare.sh
+++ overlay/common/root/prepare.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
 INI=/var/www/html/ExtraLocalConfig.ini
-[ -e "$INI" ] && exit 0
+if [ ! -e "$INI" ]; then
 
-read -r -p "Enter google email username: " SMTP_USER
-read -r -s -p "Enter google email password: " SMTP_PASS
+    [ -z "$SMTP_USER" ] && read -r -p "Enter google email username: " SMTP_USER
+    [ -z "$SMTP_PASS" ] && read -r -s -p "Enter google email password: " SMTP_PASS
 
-cat << EOF > $INI
+    cat <<- EOF > $INI
 smtp_user = '$SMTP_USER'
 smtp_pass = '$SMTP_PASS'
 EOF
+fi
+
+if [ ! -e /etc/apache2/ssl.crt ]; then
+    [ -z "$SERVER_DOMAIN" ] && read -r -p "Enter the server domain: " SERVER_DOMAIN
+    eval "cat <<EOF
+$(</root/ssl.csr.tpl)
+EOF
+" 2> /dev/null > /root/ssl.csr
+
+    openssl req -new -nodes -x509 -days 36500 -keyout /etc/apache2/ssl.key -out /etc/apache2/ssl.crt -config /root/ssl.csr
+fi
