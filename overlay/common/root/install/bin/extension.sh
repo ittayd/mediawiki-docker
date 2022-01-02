65c1d830f80c5257f64ae00bdf996e5391d31f7c
Ittay Dror <idror@akamai.com>
2022-01-02 16:02:29 +0200

    added extensions, fixed ownerships, made extension installation standard

--- overlay/common/root/install/bin/extension.sh
+++ overlay/common/root/install/bin/extension.sh
@@ -1,8 +1,30 @@
 #! /bin/bash
 
-echo "Installing $1"
-TMP=/tmp/extension-$1.tar.gz
-MAGIC_NUMBER=$(curl -s https://extdist.wmflabs.org/dist/extensions/ | grep -o -P "(?<=$1-REL${MEDIAWIKI_VERSION_MAJOR}_${MEDIAWIKI_VERSION_MINOR}-)[0-9a-z]{7}(?=.tar.gz)" | head -1)
-curl -s -o $TMP https://extdist.wmflabs.org/dist/extensions/$1-REL${MEDIAWIKI_VERSION_MAJOR}_${MEDIAWIKI_VERSION_MINOR}-${MAGIC_NUMBER}.tar.gz \
-&& tar -xzf $TMP -C /var/www/html/extensions \
-&& rm $TMP
+function run() {
+    local cmd=$(which $1)
+    shift
+    su -s $cmd www-data -- "$@"
+}
+
+NAME=$1
+TYPE=$2
+
+echo "Installing $NAME"
+case $TYPE in
+    skins | extensions)
+        TMP=/tmp/$TYPE-$NAME.tar.gz
+        MAGIC_NUMBER=$(curl -s https://extdist.wmflabs.org/dist/$TYPE/ | grep -o -P "(?<=>$NAME-REL${MEDIAWIKI_VERSION_MAJOR}_${MEDIAWIKI_VERSION_MINOR}-)[0-9a-z]{7}(?=\.tar\.gz</a>)" | head -1)
+        curl -s -o $TMP https://extdist.wmflabs.org/dist/$TYPE/$NAME-REL${MEDIAWIKI_VERSION_MAJOR}_${MEDIAWIKI_VERSION_MINOR}-${MAGIC_NUMBER}.tar.gz \
+        && run tar -xzf $TMP -C /var/www/html/$TYPE \
+        && rm $TMP
+        ;;
+    targz)
+        URL=$3
+        run mkdir -p /var/www/html/extensions/$NAME 
+        curl -Ls $URL | run tar xz --strip=1 -C /var/www/html/extensions/$NAME
+        ;;
+    composer)
+        PKG=$3
+        run composer require $PKG
+        ;;
+esac
